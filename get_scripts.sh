d01de262800d8c70168ed40ea72baf8d24f6a971
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-27 10:26:13 +0000

    Add files via upload

---
+++ get_scripts.sh
@@ -0,0 +1,39 @@
+export username=$1
+export password=$2
+
+
+list_script=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select script_name from scripts;"  ) )
+list_checksum=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select CHECKSUM_VALUE from scripts;"  ) )
+
+
+for f in PATT_UTILS/sql/*; do
+	script_name=$(echo $f| cut -d'/' -f 3)
+	CHECKSUM_VALUE=`md5sum $f | awk '{print $1}'`
+	echo $script_name
+	echo $CHECKSUM_VALUE
+	SCRIPT_NAME_UPPERCASE=$(echo $script_name | tr '[:lower:]' '[:upper:]')
+	DB_NAME_IN_SCRIPT_UPPERCASE=`Read_DB_Name $f 1`
+	
+	if [[ ${list_script[*]} =~ "$script_name" ]] && [[ ${list_checksum[*]} =~ "$CHECKSUM_VALUE" ]]
+	then
+		echo "pas de nouveau scripts"
+		# exit 1
+		
+	elif [[ ${list_script[*]} =~ "$script_name" ]] && [[ !(${list_checksum[*]} =~ "$CHECKSUM_VALUE") ]]
+	then
+		echo "le script $script_name est changer "
+		mysql -u$username -p$password -Bse "use db5;update scripts set  CHECKSUM_VALUE = '$CHECKSUM_VALUE', script_handled ='encour' where script_name='$script_name';"
+	else
+		if [[ $SCRIPT_NAME_UPPERCASE =~ $SCRIPT_NAME_REGEXP ]] || [[ $SCRIPT_NAME_UPPERCASE = *"$DB_NAME_IN_SCRIPT_UPPERCASE"* ]]
+		then
+
+			echo $DB_NAME_IN_SCRIPT_UPPERCASE $SCRIPT_NAME_UPPERCASE
+			mysql -u$username -p$password -Bse "use db5;insert into scripts (script_name,script_handled,CHECKSUM_VALUE) values('$script_name','encour','$CHECKSUM_VALUE');"
+		else
+			echo "Le fichier $script_name ne peut etre tester car il est mal nomme ou contient une incoherence au niveau du nom de la base de donnees" 
+		fi
+	fi
+
+									
+done
+
