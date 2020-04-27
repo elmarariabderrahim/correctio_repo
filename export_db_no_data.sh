7f9e625a185659b01cf376724d82e108317d1e41
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-27 13:42:47 +0000

    Update export_db_no_data.sh

--- export_db_no_data.sh
+++ export_db_no_data.sh
@@ -1,6 +1,6 @@
 export username=$1
 export password=$2
-list_script_alredy_succes=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select script_name from scripts where script_state='succes' and script_handled !='traite';"  ) )
+list_script_alredy_succes=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select script_name from scripts where script_state='succes';"  ) )
 
 
 list_database_in_script=()
