ef94f6775dd2c6e965302251fb563d84d1537829
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-27 12:17:32 +0000

    Update get_scripts.sh

--- get_scripts.sh
+++ get_scripts.sh
@@ -1,10 +1,35 @@
 export username=$1
 export password=$2
 
+SCRIPT_NAME_REGEXP='^([0-9]){3,4}(_[0-9]){0,1}_([A-Z]){3,4}(_PF([-_](DEV|INT|INT0|INT1|INT2|INT3|INT4|INT5|REC|REC1|REC2|RCT|RCT1|RCT2|TEST|PROD))+){0,1}_(PIXID|DWHSTAGE|DWHTMP|PROVIDER|MISSION){1}_(TI[-_][0-9]{1,10})(_ST[-_][0-9]{1,10})?_([A-Z0-9_-]+)(\.SQL)$'
 
 list_script=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select script_name from scripts;"  ) )
 list_checksum=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select CHECKSUM_VALUE from scripts;"  ) )
-
+PIPE="|"
+Read_DB_Name() {
+	DB_INSTRUCTION="NODBINSTRUCTIONINTHEFILE"
+	while read -r line
+	do
+    	TEMP=$(echo $line | tr '[:lower:]' '[:upper:]')
+    	#echo "line $TEMP"
+    	if [[ $TEMP = *"USE"* ]]; then
+    		if [[ $2 -eq 1 ]]; then
+    			DB_INSTRUCTION=$TEMP
+    		else 
+    			DB_INSTRUCTION=$line
+    		fi
+    		break
+    	fi
+	done < "$1"
+	RESULT=""
+	if [[ $DB_INSTRUCTION = *"\`"* ]] 
+		then 
+		RESULT=$(echo $DB_INSTRUCTION | cut -d"\`" -f2 | xargs)
+	else
+		RESULT=$(echo $DB_INSTRUCTION | cut -d" " -f2 | cut -d";" -f1 | xargs)
+	fi
+  	echo -e "${RESULT}"
+}
 
 for f in PATT_UTILS/sql/*; do
 	script_name=$(echo $f| cut -d'/' -f 3)
@@ -34,6 +59,6 @@ for f in PATT_UTILS/sql/*; do
 		fi
 	fi
 
-									
+							
 done
 
